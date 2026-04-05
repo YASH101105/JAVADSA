849fe0b53372324cf9947b9fa331e4bba523282e
YASH RASTOGI <rastogiy60@gmail.com>
2026-04-05 18:34:54 +0530

    Duplicate element in the array for the subset

---
+++ RECURSION/src/DuplicateSequence.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DuplicateSequence {
+    public static void main(String[] args) {
+       int[] arr = {1,2,3};
+        List<List<Integer>> ans = subsetDuplicate(arr);
+        for (List<Integer> list :ans) {
+            System.out.println(list);
+        }
+    }//when you find a duplicate element , only add it in the newly created subtance in the previous steps
+     static List<List<Integer>> subsetDuplicate(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer . add(new ArrayList<>());
+        int start = 0;
+        int end  = 0 ;
+        for (int i = 0 ; i<arr.length ; i++){
+            start = 0 ;
+            //duplicate
+            if(i>0&& arr[i]==arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size()-1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal  = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+
+            }
+
+            }
+        return outer;
+         }
+     }
